f816276413f1e910fce14c5bf5e8005fe538dfdc
emma673r <90177139+emma673r@users.noreply.github.com>
2024-03-13 11:37:25 +0100

    and so it works

--- src/main/java/com/example/listsandforms/controllers/ValidateController.java
+++ src/main/java/com/example/listsandforms/controllers/ValidateController.java
@@ -1,17 +1,29 @@
 package com.example.listsandforms.controllers;
 
+import com.example.listsandforms.models.Something;
+import com.example.listsandforms.utils.ListCreator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+
 @Controller
 public class ValidateController {
+    @Autowired
+    private ListCreator listCreator;
+
     @PostMapping("/validate")
     public String validate(Model model, @RequestParam String name, @RequestParam String password){
         if (name.equals(password)){
             model.addAttribute("message", "Right!");
-            return "home/login";
+
+            ArrayList<Something> list = listCreator.getList();
+            model.addAttribute("list", list);
+
+            return "home/lists";
         }
         else {
             model.addAttribute("message", "Wrong!");
